e829783c22656317bd8b9f6d7575180718ffd313
Shane Staret <shanestaret@gmail.com>
2018-04-14 18:06:06 -0400

    Added Alert superclass and updated StudentAlert

--- TuTum/src/StudentAlert.java
+++ TuTum/src/StudentAlert.java
@@ -1,43 +1,13 @@
 import java.time.LocalDateTime;
 
-public class StudentAlert {
+public class StudentAlert extends Alert {
 	//level of danger
-	private int level;
-	private String message = "";
-	
-	private LocalDateTime date;
 	
 	//constructor
 	public StudentAlert(int level, String message, LocalDateTime date) {
-		this.level = level;
-		this.message = message;
-		this.date = date;
+		super(level, message, date);
 	}
 	//print method
-	public String toString() {
-		return (level + "/t" + date.toString());
-	}
-	//getter for date
-	public LocalDateTime getDate() {
-		return date;
-	}
-	//getter for level
-	public int getLevel() {
-		return level;
-	}
-	//method to change level
-	public void resolveAlert() {
-		this.level = 0;
-	}
-	//
-	public void increaseLevel() {
-		if(this.level < 3)
-			this.level++;
-	}
-	//method to write message
-	public void writeMessage(String message) {
-		this.message = message;
-	}
 	
 	
 }
