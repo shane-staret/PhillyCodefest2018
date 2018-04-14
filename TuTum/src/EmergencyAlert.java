785ba6ad3fec72eee67965699e5c2c87281d1f8d
Unknown <carloschavez_07@yahoo.com>
2018-04-14 14:33:37 -0400

    alert class and subclasses finished

---
+++ TuTum/src/EmergencyAlert.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+
+public class EmergencyAlert extends Alert {
+	
+	//constructor
+	public EmergencyAlert(LocalDateTime date) {
+		super(3, date);
+	}
+	
+	
+
+}
---
+++ TuTum/src/PotentialAlert.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class PotentialAlert extends Alert{
+	//constructor
+	public PotentialAlert(LocalDateTime date) {
+	super(1, date);
+	}
+}	
+
