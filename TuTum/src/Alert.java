efb7b7ac58300788193cfad664155e58954e2fda
Shane Staret <shanestaret@gmail.com>
2018-04-14 18:52:08 -0400

    Updated Alert and SecurityGuard classes

--- TuTum/src/Alert.java
+++ TuTum/src/Alert.java
@@ -1,4 +1,50 @@
+import java.time.LocalDateTime;
 
 public class Alert {
-
+	private int level;
+	private String message = "";
+	private LocalDateTime date;
+	
+	public Alert(int level, String message, LocalDateTime date) {
+		this.level = level;
+		this.message = message;
+		this.date = date;
+	}
+	
+	public Alert(int level, LocalDateTime date) {
+		this.level = level;
+		this.date = date;
+	}
+	
+	//getter for date
+	public LocalDateTime getDate() {
+		return date;
+	}
+	//getter for level
+	public int getLevel() {
+		return level;
+	}
+	//method to change level
+	public void resolveAlert() {
+		this.level = 0;
+	}
+	//
+	public void increaseLevel() {
+		if(this.level < 3)
+			this.level++;
+	}
+	public void decreaseLevel() {
+		if(this.level > 0)
+			this.level--;
+	}
+	
+	
+	//method to write message
+	public void writeMessage(String message) {
+		this.message = message;
+	}
+	
+	public String toString() {
+		return (level + "\t" + message + "\t" + date.toString());
+	}
 }
