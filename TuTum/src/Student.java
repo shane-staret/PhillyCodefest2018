7171b589e1b92415efa4f512ecebfca2584b372b
Shane Staret <shanestaret@gmail.com>
2018-04-14 17:30:13 -0400

    Updated Student and StudentAlert class

--- TuTum/src/Student.java
+++ TuTum/src/Student.java
@@ -1,30 +1,23 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-
-/*
- * Name (String)
-ID number (int)
-allStudentLocations (ArrayList<String>)
-Current Location (string)
-Status Flag (boolean)
-Getters
-studentLocationStatus Method
-
- */
 
 public class Student {
 	private String studentID;
-	private String studentName;
+	private String studentFirstName;
+	private String studentLastName;
 	private String studentCurrentLocation; //last known location of student
 	private String studentPreviousLocation; //previous location of student
-	private String absenceAlert; //eventually will be an Alert object
-	private boolean studentIsAbsent; //true: student is absent from building; false; student is not absent from building
-	private ArrayList<String> allStudentLocations = new ArrayList<>(); //holds all locations student has been logged at for the day
+	private String absenceAlertMessage; //holds message of alert
+	private int absenceAlertLevel; //holds level of alert
+	private boolean studentIsAbsent = true; //true: student is absent from building; false; student is not absent from building
 	private boolean studentStatusFlag; //true: student is seen as harmful; false: student is not seen as harmful
+	private LocalDateTime date;
 	
-	public Student(String studentID, String studentName, boolean studentStatusFlag) {
-		this.setStudentID(studentID);
-		this.setStudentName(studentName);
-		this.setStudentStatusFlag(studentStatusFlag);
+	public Student(String studentID, String studentFirstName, String studentLastName, boolean studentStatusFlag) {
+		this.studentID = studentID;
+		this.studentFirstName = studentFirstName;
+		this.studentLastName = studentLastName;
+		this.studentStatusFlag = studentStatusFlag;
 	}
 
 	public String getStudentID() {
@@ -35,58 +28,68 @@ public class Student {
 		this.studentID = studentID;
 	}
 
-	public String getStudentName() {
-		return studentName;
-	}
-
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-
 	public String getStudentCurrentLocation() {
 		return studentCurrentLocation;
+	}
+	
+	public String getStudentFirstName() {
+		return studentFirstName;
+	}
+
+	public void setStudentFirstName(String studentFirstName) {
+		this.studentFirstName = studentFirstName;
+	}
+
+	public String getStudentLastName() {
+		return studentLastName;
+	}
+
+	public void setStudentLastName(String studentLastName) {
+		this.studentLastName = studentLastName;
 	}
 
 	//sets current location of student to new value based on most recent attendance by teacher (Teacher class needs to access this)
 	public void setStudentCurrentLocation(String studentCurrentLocation) {
-		if(!allStudentLocations.isEmpty())
-			studentPreviousLocation = studentCurrentLocation;
+		if(studentCurrentLocation != null)
+			studentPreviousLocation = this.studentCurrentLocation;
 		this.studentCurrentLocation = studentCurrentLocation;
-		allStudentLocations.add(studentCurrentLocation);
+		studentLocationStatus();
 	}
 
 	public boolean getStudentStatusFlag() {
 		return studentStatusFlag;
 	}
 	
-	//sets current status flag of student to new value due to an incident (SecurityGuard class must access this)
+	//sets current status flag of student to new value due to an incident (Teacher class must access this)
 	public void setStudentStatusFlag(boolean studentStatusFlag) {
 		this.studentStatusFlag = studentStatusFlag;
 	}
 	
+	//checks student's most recent and previous location; if student was in previous class but not in current, alert creation method is called
 	private void studentLocationStatus() {
-		for(int i = 0; i < allStudentLocations.size(); i++) {
-			if(!allStudentLocations.get(i).equals("unknown")) {
+		if(!studentPreviousLocation.equals("unknown") && studentCurrentLocation.equals("unknown")) {
 				studentIsAbsent = false;
-				if(i == allStudentLocations.size() - 1 && !(allStudentLocations.get(allStudentLocations.size() - 1)).equals("unknown"))
-					absenceAlert = "Present, but student is not in current class. Last known location: " + studentPreviousLocation;
+				if(studentStatusFlag)
+					absenceAlertLevel = 2;
+				else
+					absenceAlertLevel = 1;
+				absenceAlertMessage = "Present, but student is not in current class. Last known location: " + studentPreviousLocation;
+				date = java.time.LocalDateTime.now();
+				getAbsenceAlert();
+
 			}
-			
-		}
-		studentIsAbsent = true;
-		absenceAlert = "Absent, student was never in building";
+		else if(!studentPreviousLocation.equals("unknown") && !studentCurrentLocation.equals("unknown"))
+			studentIsAbsent = false;
 	}
 
 	//true: student was never in building; false: student in building at some point
 	public boolean isStudentAbsent() {
-		studentLocationStatus();
 		return studentIsAbsent;
 	}
 	
-	//will eventually return an Alert object
-	public String getAbsenceAlert() {
-		return absenceAlert;
+	//creates StudentAlert object to be added to database and dealt with
+	public StudentAlert getAbsenceAlert() {
+		studentLocationStatus();
+		return new StudentAlert(absenceAlertLevel, absenceAlertMessage, date);
 	}
-	
-	
 }
--- TuTum/src/StudentAlert.java
+++ TuTum/src/StudentAlert.java
@@ -1,6 +1,6 @@
 import java.time.LocalDateTime;
 
-public class Alert {
+public class StudentAlert {
 	//level of danger
 	private int level;
 	private String message = "";
@@ -8,8 +8,9 @@ public class Alert {
 	private LocalDateTime date;
 	
 	//constructor
-	public Alert(int level, LocalDateTime date) {
+	public StudentAlert(int level, String message, LocalDateTime date) {
 		this.level = level;
+		this.message = message;
 		this.date = date;
 	}
 	//print method
