a3582b082339cc9ea5304a7617e761e3a5ec37c0
Unknown <carloschavez_07@yahoo.com>
2018-04-15 00:46:28 -0400

    Login interface start

---
+++ TuTum/src/LoginInterface.java
@@ -0,0 +1,55 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class LoginInterface extends JFrame {
+	//instance variable section -- GUI components
+	 private JLabel lblUsername, lblPassword;
+	 private JTextField txtUsername, txtPassword;
+	 private JButton btnLogin;
+	 private JPanel pnlUsername, pnlButton, pnlPassword;
+	 
+	 //constructor
+	 public LoginInterface() {
+		  setTitle("Login");  
+	      setSize(350, 200);   
+	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	      buildContents();
+	      pack(); // optional(gets rid of whitespace)
+	      setVisible(true); 
+	   }
+	 public void buildContents() {
+		 //initializing instance variables
+		 lblUsername = new JLabel("Username: ");
+		 txtUsername = new JTextField(10);
+		 
+		 lblPassword = new JLabel("Password: ");
+		 txtPassword = new JTextField(10);
+		 
+		 btnLogin = new JButton("Login");
+		 //add actionListener here
+		 pnlPassword = new JPanel();
+		 pnlUsername = new JPanel();
+		 pnlButton = new JPanel();
+		 //adding panels to frame
+		 pnlUsername.add(lblUsername);
+		 pnlUsername.add(txtUsername);
+		 
+		 pnlPassword.add(lblPassword);
+		 pnlPassword.add(txtPassword);
+		 
+		 pnlButton.add(btnLogin);
+		 
+		 //adding panels to frame
+		 setLayout(new GridLayout(3,1));
+		 add(pnlUsername);
+		 add(pnlPassword);
+		 add(pnlButton);
+		 
+		 
+		 
+	 }
+	 public static void main(String[] args) {
+		 new LoginInterface();
+	 }
+}
